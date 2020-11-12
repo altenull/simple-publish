a4ea75707fd61bfcb402adcc905bd9b0a63206a2
Heonyoung Kim <altenull@gmail.com>
2020-11-13 00:46:23 +0900

    feat: import NewRepositoryPage to App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { NewRepositoryPage } from "./components/pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main style={{ padding: "96px 0px 0" }}>
+      <NewRepositoryPage />
+    </main>
   );
 }
 
