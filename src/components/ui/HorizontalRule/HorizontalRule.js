994a0b61896d818e0af31ff940dbe4cb86e216a0
Heonyoung Kim <altenull@gmail.com>
2020-11-12 23:28:08 +0900

    feat(ui): create HorizontalRule component

---
+++ src/components/ui/HorizontalRule/HorizontalRule.js
@@ -0,0 +1,8 @@
+import { React } from "react";
+import "./HorizontalRule.css";
+
+const HorizontalRule = () => {
+  return <hr aria-hidden="true" className="horizontal-rule" />;
+};
+
+export default HorizontalRule;
--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from "./Button/Button";
 export { default as Checkbox } from "./Checkbox/Checkbox";
 export { default as Dropdown } from "./Dropdown/Dropdown";
+export { default as HorizontalRule } from "./HorizontalRule/HorizontalRule";
 export { default as Input } from "./Input/Input";
 export { default as RadioButton } from "./RadioButton/RadioButton";
