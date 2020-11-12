ff7fb626f668ec2a6a8667083b2a0a97dadaedf7
Heonyoung Kim <altenull@gmail.com>
2020-11-13 00:43:28 +0900

    feat(ui): add label prop to Dropdown

--- src/components/ui/Dropdown/Dropdown.js
+++ src/components/ui/Dropdown/Dropdown.js
@@ -2,8 +2,13 @@ import { React } from "react";
 import "./Dropdown.css";
 import { Button } from "../../ui";
 
-const Dropdown = ({ children }) => {
-  return <Button className="dropdown">{children}</Button>;
+const Dropdown = ({ children, label }) => {
+  return (
+    <div className="dropdown-wrapper">
+      <label className="dropdown-label">{label}</label>
+      <Button className="dropdown">{children}</Button>
+    </div>
+  );
 };
 
 export default Dropdown;
