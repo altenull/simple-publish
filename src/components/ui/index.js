4ce9fc144d1feb809a6b563b63df20b6e03b17bc
Heonyoung Kim <altenull@gmail.com>
2020-11-13 10:32:48 +0900

    feat(ui): create BookIcon and LockIcon components

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -2,5 +2,7 @@ export { default as Button } from "./Button/Button";
 export { default as Checkbox } from "./Checkbox/Checkbox";
 export { default as Dropdown } from "./Dropdown/Dropdown";
 export { default as HorizontalRule } from "./HorizontalRule/HorizontalRule";
+export { default as BookIcon } from "./Icons/BookIcon";
+export { default as LockIcon } from "./Icons/LockIcon";
 export { default as Input } from "./Input/Input";
 export { default as RadioButton } from "./RadioButton/RadioButton";
