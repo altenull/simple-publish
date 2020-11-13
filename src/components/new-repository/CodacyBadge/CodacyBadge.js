cef9dc0ec64014e9585f58e51a7b360828278aa2
Heonyoung Kim <altenull@gmail.com>
2020-11-13 11:05:55 +0900

    feat(new-repository): create CodacyBadge commponent

---
+++ src/components/new-repository/CodacyBadge/CodacyBadge.js
@@ -0,0 +1,16 @@
+import { React } from "react";
+import "./CodacyBadge.css";
+
+const CodacyBadge = () => {
+  return (
+    <div className="codacy-badge">
+      <img
+        className="codacy-icon"
+        src="https://avatars0.githubusercontent.com/ml/43?s=106&amp;v=4"
+        alt="Codacy logo preview"
+      />
+    </div>
+  );
+};
+
+export default CodacyBadge;
--- src/components/new-repository/index.js
+++ src/components/new-repository/index.js
@@ -1,2 +1,3 @@
+export { default as CodacyBadge } from "./CodacyBadge/CodacyBadge";
 export { default as ContentsHeader } from "./ContentsHeader/ContentsHeader";
 export { default as OwnerDropdown } from "./OwnerDropdown/OwnerDropdown";
