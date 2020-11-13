e0434f45c772945c5f53d0c4f80b1f601b3a6172
Heonyoung Kim <altenull@gmail.com>
2020-11-13 11:12:25 +0900

    feat(ui): being able to have icon prop to Checkbox component

--- src/components/ui/Checkbox/Checkbox.js
+++ src/components/ui/Checkbox/Checkbox.js
@@ -1,13 +1,25 @@
 import { React } from "react";
 import "./Checkbox.css";
 
-const Checkbox = ({ label, description }) => {
+const Checkbox = ({ label, description, icon }) => {
+  const hasIcon = icon != null;
+
   return (
-    <div className="checkbox-wrapper">
+    <div
+      className={`checkbox-wrapper${
+        hasIcon ? " checkbox-wrapper-with-icon" : ""
+      }`}
+    >
       <label className="checkbox-label">
-        <input type="checkbox" className="checkbox" />
+        <input
+          type="checkbox"
+          className={`checkbox${hasIcon ? " checkbox-with-icon" : ""}`}
+        />
         {label}
       </label>
+
+      {hasIcon && <span className="checkbox-icon">{icon}</span>}
+
       {description != null && (
         <div className="checkbox-description">{description}</div>
       )}
